9f8f748d14bd02c99f9be482da2a4f3d1b710491
史远 <shiyuan@films.cn>
2019-06-11 11:19:48 +0800

    '添加说明'

--- app.js
+++ app.js
@@ -24,14 +24,14 @@ App({
         // 获取用户的当前设置
         wx.getSetting({
             success: res => {
-                //authSetting--用户授权结果
-                // scope.userInfo  对应接口 wx.getUserInfo 用户信息
+                // authSetting--用户授权结果
+                // 当授权给一个 scope 之后，其对应的所有接口都可以直接使用
+                // scope.userInfo--用户信息，对应--wx.getUserInfo
                 if (res.authSetting['scope.userInfo']) {
                     // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
                     wx.getUserInfo({
                         success: res => {
                             // 可以将 res 发送给后台解码出 unionId
-                            // console.log(res)
                             this.globalData.userInfo = res.userInfo
                             // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
                             // 所以此处加入 callback 以防止这种情况
