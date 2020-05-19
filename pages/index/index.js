357ea518b07f26ca434762f0f7815ed3b3b82eaf
shiyuan <shiyuan@films.cn>
2020-05-19 16:27:28 +0800

    '2020.5.19'

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,7 +15,6 @@ Page({
     })
   },
   onLoad: function() {
-    console.log(app.globalData.userInfo)
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -23,7 +22,7 @@ Page({
       })
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
+      // 所以加入 callback 防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
@@ -44,7 +43,7 @@ Page({
     }
   },
   getUserInfo: function(e) {
-    console.log(e)
+    console.log('用户授权信息',e.detail.userInfo)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
